3f6d0c62e44fcab10ede96bbcfaccffa9a89d874
borisbolsh <bolsh.bo@yandex.ru>
2022-03-02 23:47:25 +0300

    business app upd main screen

--- BusinessApp/BusinessApp/ViewController.h
+++ BusinessApp/BusinessApp/ViewController.h
@@ -1,7 +1,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *servicesBtn;
+@property (weak, nonatomic) IBOutlet UIButton *portfolioBtn;
+@property (weak, nonatomic) IBOutlet UIButton *aboutUsBtn;
+@property (weak, nonatomic) IBOutlet UIButton *contactUsBtn;
+@property (weak, nonatomic) IBOutlet UIButton *socialLinksBtn;
 
+- (IBAction)servicesTapped:(id)sender;
+- (IBAction)portfolioTapped:(id)sender;
+- (IBAction)aboutUsTapped:(id)sender;
+- (IBAction)contactUsTapped:(id)sender;
 
 @end
 
--- BusinessApp/BusinessApp/ViewController.m
+++ BusinessApp/BusinessApp/ViewController.m
@@ -8,9 +8,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 
+    UIView *statusBar = [[UIView alloc]initWithFrame:[UIApplication sharedApplication].keyWindow.windowScene.statusBarManager.statusBarFrame] ;
+    statusBar.backgroundColor = [UIColor colorWithRed: 55.0/255.0 green:86.0/255.0 blue:135.0/255.0 alpha:1.0];
+    [[UIApplication sharedApplication].keyWindow addSubview:statusBar];
+
+    self.servicesBtn.layer.cornerRadius = 5;
+    self.portfolioBtn.layer.cornerRadius = 5;
+    self.aboutUsBtn.layer.cornerRadius = 5;
+    self.contactUsBtn.layer.cornerRadius = 5;
+    self.socialLinksBtn.layer.cornerRadius = 5;
 }
 
 
+- (IBAction)contactUsTapped:(id)sender {
+    [(UITabBarController *) self.tabBarController setSelectedIndex:4];
+}
+
+- (IBAction)aboutUsTapped:(id)sender {
+    [(UITabBarController *) self.tabBarController setSelectedIndex:1];
+}
+
+- (IBAction)portfolioTapped:(id)sender {
+    [(UITabBarController *) self.tabBarController setSelectedIndex:3];
+}
+
+- (IBAction)servicesTapped:(id)sender {
+    [(UITabBarController *) self.tabBarController setSelectedIndex:2];
+}
 @end
